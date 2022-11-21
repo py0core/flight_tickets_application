621e21684f8bae9b15e54824d98d75f4b68d14ee
Daniel D <daniknikita1816@gmail.com>
2022-11-21 22:11:25 +0200

    Removed commented lines.

--- assets/db_connector/main.py
+++ assets/db_connector/main.py
@@ -2,15 +2,15 @@
 
 import db_connector
 
-# """PARSING DATA INTO DB.JASON"""
-# email: str = "test@test.com"
-# f_name: str = "test"
-# l_name: str = "testovich"
-# password: str = "123$"
-# try:
-#     db_connector.write_data({email:{"first_name":f_name,"last_name":l_name,"pass":password}}, "accounts")
-# except:
-#     print("ERROR")
+"""PARSING DATA INTO DB.JASON"""
+email: str = "test@test.com"
+f_name: str = "test"
+l_name: str = "testovich"
+password: str = "123$"
+try:
+    db_connector.write_data({email: {"first_name": f_name, "last_name": l_name, "pass": password}}, "accounts")
+except:
+    print("ERROR")
 
 """GETTING DATA FROM DB.JASON"""
 email: str = "test@yooho.org"
